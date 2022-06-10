678141daad766d96679e50f195ff1c20cc85d95c
Michael D <michael@codetropolis.com>
2022-06-10 05:40:07 -0500

    GoalInput modal styles.

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -16,7 +16,14 @@ export function GoalInput(props) {
         <Modal visible={props.visible} animationType='slide'>
             <View style={styles.inputContainer}>
                 <TextInput style={styles.textInput} placeholder="Your goal" onChangeText={goalInputHandler} value={enteredGoalText}/>
-                <Button title="Add goal" onPress={addGoalHandler} />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title="Add goal" onPress={addGoalHandler} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Cancel" />
+                    </View>
+                </View>
             </View>
         </Modal>
     );
@@ -28,18 +35,25 @@ const styles = StyleSheet.create({
        // View tag uses Flexbox by default.
        // Flexbox organizes by column by default.
       flex: 1, // 1 of X where X is the total number of flex:N of sibling classes.
-      flexDirection: 'row',
-      justifyContent: 'space-between',
+      justifyContent: 'center', // Aligns items vertically.
       alignItems: 'center', // fixes Add Goal button height
       marginBottom: 24,
       borderBottomWidth: 1,
       borderBottomColor:'gray',
+      padding:16,
     },
     textInput: {
       borderWidth: 1,
       borderColor: '#cccccc',
-      width: '70%',
-      marginRight: 8,
+      width: '100%',
       padding: 8,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginTop: 16,
+    },
+    button: {
+        width: '30%',
+        marginHorizontal: 8,
     },
   });
